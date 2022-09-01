bb4d8fbfb659b26f4b87bc7b09bce0939b10e950
Ken Rolloque <90883172+KenRolloque@users.noreply.github.com>
2022-09-01 22:38:20 +0800

    Add files via upload

---
+++ CODE/header.js
@@ -0,0 +1,9 @@
+function showSide(){
+    // const showHeader = document.getElementById("side-nav-id");
+    // showHeader.style.display = "inline-flex";
+
+    const element = document.getElementById("side-nav-id");
+    element.classList.toggle("active");
+
+  
+}
